82a4fc00fb0c7bf8dc1d329d7980b930f4e1dfb1
Bikash <thebikashpokhrel@gmail.com>
2024-05-17 00:19:46 +0545

    Few changes in controller and middleware logic

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,108 +1,93 @@
 import mongoose from "mongoose";
-import jwt from "jwt";
 import bcrypt from "bcrypt";
-const userSchema=new Schema({
-    username:{
-        type: 'string',
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim:true,
-        index:true
+import jwt from "jsonwebtoken";
+import { Schema } from "mongoose";
+const userSchema = new Schema(
+  {
+    username: {
+      type: "string",
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
-    email:
-    {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim:true,
-
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
-    fullname:
-    {
-        type: String,
-        required: true,
-        index:true,
-        trim:true,
+    fullname: {
+      type: String,
+      required: true,
+      index: true,
+      trim: true,
     },
-    avatar:
-    {
-        type:String, //cloudinary url
-        required:true,
+    avatar: {
+      type: String, //cloudinary url
+      required: true,
     },
-     coverImage:
-    {
-        type:String, //cloudinary url
+    coverImage: {
+      type: String, //cloudinary url
     },
-    watchHistory:[
-    {
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Video"
-    }
-],
-     password:
-     {
-        type:String,
-        required:[true,"password is required"],
-
-     },
-     refreshToken:
-     {
-        type:true,
-
-     }
-
-
-
-},{timestamps:true});
+    watchHistory: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    refreshToken: {
+      type: Boolean,
+    },
+  },
+  { timestamps: true }
+);
 
 // adding middleware before saving the password in database by hashing
-userSchema.pre("save", async function(next){
-    if (!this.isModified("password")) return next();
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
-    this.password= await bcrypt.hash(this.password,8);
-    next();
-
-
-})
+  this.password = await bcrypt.hash(this.password, 8);
+  next();
+});
 
 //checking the password validation using custom methods
-userSchema.methods.isPasswordCorrect=async function(password)
-{
-    return await bcrypt.compare(password,this.password)
-    //return boolean value 
-}
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+  //return boolean value
+};
 
-userSchema.methods.generateAccessToken= function(){
-    return jwt.sign(
-        {
-            _id:this._d,
-            email:this.email,
-            username:this.username,
-            fullname:this.fullname,
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._d,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
 
-        }
-    )
-    
-}
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._d,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_SECRET,
+    }
+  );
+};
 
-userSchema.methods.generateRefreshToken= function(){
-    return jwt.sign(
-        {
-            _id:this._d,
-            
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        {
-            expiresIn:process.env.REFRESH_TOKEN_SECRET
-
-        })
-}
-
-
-export const user= Schema.model("user",userSchema);
+export const user = mongoose.model("user", userSchema);
